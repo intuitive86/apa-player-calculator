da703bbfa0cbb8505d4a4b0655175507aa7da63a
Mikel <mikel.tan86@gmail.com>
2024-07-29 23:31:50 -0700

    made player info red to make visible for now

--- components/PlayerList.js
+++ components/PlayerList.js
@@ -1,38 +1,38 @@
 import { View, Text, StyleSheet } from "react-native";
 
 const PlayerList = ({ players }) => {
-    return (
-        <View style={styles.container}>
-            {players.map((player) => (
-                <View key={player.id} style={styles.playerRow}>
-                    <Text style={styles.playerName}>{player.name}</Text>
-                    <Text style={styles.playerLevel}>{player.level}</Text>
-                </View>
-            ))}
+  return (
+    <View style={styles.container}>
+      {players.map((player) => (
+        <View key={player.id} style={styles.playerRow}>
+          <Text style={styles.playerName}>{player.name}</Text>
+          <Text style={styles.playerLevel}>{player.level}</Text>
         </View>
-    );
+      ))}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        marginTop: 20,
-    },
-    playerRow: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderBottomWidth: 1,
-        borderBottomColor: "#ccc",
-    },
-    playerName: {
-        fontSize: 18,
-        color: "#000",
-    },
-    playerLevel: {
-        fontSize: 18,
-        color: "#000",
-    },
+  container: {
+    marginTop: 20,
+  },
+  playerRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  playerName: {
+    fontSize: 18,
+    color: "red", // Making the color red to make player info visible for now
+  },
+  playerLevel: {
+    fontSize: 18,
+    color: "red", // Making the color red to make player info visible for now
+  },
 });
 
 export default PlayerList;
