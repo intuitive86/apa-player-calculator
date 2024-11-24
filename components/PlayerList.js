0a88b53d790eae5a63e59f98710163c08ad9198d
foragerEngineer <mikel.tan86@gmail.com>
2024-11-23 20:12:28 -0800

    background added to playerName and playerLevel

--- components/PlayerList.js
+++ components/PlayerList.js
@@ -1,13 +1,19 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import defaultPlayerButtonImage from "../assets/APA/PlayerNameBG/defaultPlayerButtonImage.png";
 
 const PlayerList = ({ players }) => {
   return (
     <View style={styles.container}>
       {players.map((player) => (
-        <View key={player.id} style={styles.playerRow}>
+        <TouchableOpacity key={player.id} style={styles.playerRow}>
+          <Image
+            source={defaultPlayerButtonImage}
+            style={styles.playerBackground}
+            resizeMode="contain"
+          />
           <Text style={styles.playerName}>{player.name}</Text>
           <Text style={styles.playerLevel}>{player.level}</Text>
-        </View>
+        </TouchableOpacity>
       ))}
     </View>
   );
@@ -20,7 +26,8 @@ const styles = StyleSheet.create({
   },
   playerRow: {
     flexDirection: "row",
-    justifyContent: "flex-start",
+    justifyContent: "center",
+    alignItems: "center",
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderBottomWidth: 1,
@@ -28,17 +35,21 @@ const styles = StyleSheet.create({
     width: "80%",
     position: "relative",
   },
+  playerBackground: {
+    position: "absolute",
+    width: "340%",
+    height: "340%",
+    zIndex: -1,
+  },
   playerName: {
     fontSize: 18,
-    color: "#000",
-    position: "absolute",
-    left: -30,
+    color: "#fff",
+    left: -60,
   },
   playerLevel: {
     fontSize: 18,
-    color: "#000",
-    position: "absolute",
-    left: 300,
+    color: "#fff",
+    left: 40,
   },
 });
 
