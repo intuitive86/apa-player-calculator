58a5eea5b9313d0dcd981c00a0349c7342d8ec85
intuitive86 <64850611+intuitive86@users.noreply.github.com>
2024-11-25 22:31:11 -0800

    implemented max value exceeded state to player (#12)
    Co-authored-by: foragerEngineer <mikel.tan86@gmail.com>

--- components/PlayerList.js
+++ components/PlayerList.js
@@ -2,12 +2,12 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import PlayerRowStateHandler from "./PlayerRowStateHandler";
 
-const PlayerList = ({ players, togglePlayerLevel }) => {
+const PlayerList = ({ players, togglePlayerLevel, isObjectiveExceeded, lastSelectedPlayerId }) => {
   return (
     <View style={styles.container}>
       {players.map((player, index) => (
         <View key={player.id} style={[styles.playerContainer, index !== players.length - 1 && styles.playerContainerSpacing]}>
-          <PlayerRowStateHandler player={player} togglePlayerLevel={togglePlayerLevel} />
+          <PlayerRowStateHandler player={player} togglePlayerLevel={togglePlayerLevel} isObjectiveExceeded={isObjectiveExceeded} lastSelectedPlayerId={lastSelectedPlayerId} />
         </View>
       ))}
     </View>
